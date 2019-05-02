de9ad0635d59bdcacdf872ea081c7b834200c644
Osaetin EVBUOMA <osaetinevbuoma@gmail.com>
2019-05-02 09:12:58 +0100

    AsyncTask and AsyncTaskLoader

---
+++ WhoWroteIt/app/src/main/java/com/modnsolutions/whowroteit/MainActivity.java
@@ -0,0 +1,63 @@
+package com.modnsolutions.whowroteit;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+    }
+
+    public void searchBooks(View view) {
+        // Get the search string from the input field.
+        String queryString = mBookInput.getText().toString();
+
+        // Hide the keyboard when the user taps the search button.
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.
+                INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.
+                    HIDE_NOT_ALWAYS);
+        }
+
+        // Get connection status.
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.
+                CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        // Check availability of network connection and that a query string is entered.
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        } else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+}
