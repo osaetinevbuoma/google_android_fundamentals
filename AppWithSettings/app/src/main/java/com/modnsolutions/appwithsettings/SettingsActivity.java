747ac6d1d25763a4e5a8e7702c8183bb6c072d8b
Osaetin EVBUOMA <osaetinevbuoma@gmail.com>
2019-05-07 13:35:48 +0100

    App Settings

---
+++ AppWithSettings/app/src/main/java/com/modnsolutions/appwithsettings/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.modnsolutions.appwithsettings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
